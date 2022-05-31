e9a58536f0a41fa164f56b7653d48af15a2504b5
ercusz <eurokungz.xkz@gmail.com>
2022-05-31 22:00:40 +0700

    test: :white_check_mark: add tests for GET,POST handlers

---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,81 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+type postData struct {
+	key   string
+	value string
+}
+
+var theTests = []struct {
+	name               string
+	url                string
+	method             string
+	params             []postData
+	expectedStatusCode int
+}{
+	{"home", "/", "GET", []postData{}, http.StatusOK},
+	{"about", "/about", "GET", []postData{}, http.StatusOK},
+	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
+	{"gq", "/generals-quarters", "GET", []postData{}, http.StatusOK},
+	{"ms", "/majors-suite", "GET", []postData{}, http.StatusOK},
+	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
+	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"post-search-av", "/search-availability", "POST", []postData{
+		{key: "start", value: "2022-05-31"},
+		{key: "end", value: "2022-06-01"},
+	}, http.StatusOK},
+	{"post-search-av-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2022-05-31"},
+		{key: "end", value: "2022-06-01"},
+	}, http.StatusOK},
+	{"post-make-reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "John"},
+		{key: "last_name", value: "Doe"},
+		{key: "email", value: "john@doe.com"},
+		{key: "phone", value: "123-456-7890"},
+	}, http.StatusOK},
+}
+
+func TestHandlers(t *testing.T) {
+	routes := getRoutes()
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range theTests {
+		if e.method == "GET" {
+			resp, err := ts.Client().Get(ts.URL + e.url)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+
+		} else {
+			values := url.Values{}
+
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+
+		}
+	}
+}
