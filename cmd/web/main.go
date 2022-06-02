cd2d8c2edb8da5b503b653663ce89c464d17d883
ercusz <eurokungz.xkz@gmail.com>
2022-06-02 16:37:45 +0700

    feat: :hammer: add helpers package for centralized error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/ercusz/go-bookings/internal/config"
 	"github.com/ercusz/go-bookings/internal/handlers"
+	"github.com/ercusz/go-bookings/internal/helpers"
 	"github.com/ercusz/go-bookings/internal/models"
 	"github.com/ercusz/go-bookings/internal/render"
 )
@@ -19,6 +21,9 @@ const portNumber = ":8080"
 var app config.AppConfig
 
 var session *scs.SessionManager
+
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -45,6 +50,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -66,6 +77,8 @@ func run() error {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplate(&app)
+
+	helpers.NewHelpers(&app)
 
 	return nil
 }
